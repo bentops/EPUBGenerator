994b51bd750bee349ebb71a896e0412c967a08da
Worawut <virus_online-555.com@hotmail.com>
2016-01-14 14:45:10 +0700

    eiei

---
+++ EPUBGenerator/EPUBGenerator/TempWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+using TTSScheduler;
+
+namespace EPUBGenerator
+{
+    /// <summary>
+    /// Interaction logic for TempWindow.xaml
+    /// </summary>
+    public partial class TempWindow : Window
+    {
+        //private Scheduler TTSSch = new Scheduler("g2pconfig_cutts_dict.conf", "SynBlock.conf");
+        private FolderBrowserDialog folderBrowserDialog;
+        private OpenFileDialog openFileDialog;
+
+
+        public TempWindow()
+        {
+            InitializeComponent();
+
+            folderBrowserDialog = new FolderBrowserDialog();
+
+            openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "EPUB files (*.epub)|*.epub";
+
+            createButton.IsEnabled = false;
+        }
+
+        private void createButton_Click(object sender, RoutedEventArgs e)
+        {
+            output.Text = TestClass.reCreate(openFileDialog.FileName, (string)projLocationPath.Content);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //TTSSch.Clear();
+            //TestClass.Save();
+        }
+        
+        private void projName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(folderBrowserDialog.SelectedPath))
+            {
+                projLocationPath.Content = System.IO.Path.Combine(folderBrowserDialog.SelectedPath, projName.Text);
+            }
+            updateCreateButton();
+        }
+
+        private void browseLocation_Click(object sender, RoutedEventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                projLocationPath.Content = System.IO.Path.Combine(folderBrowserDialog.SelectedPath, projName.Text);
+                updateCreateButton();
+            }
+        }
+
+        private void browseEpub_Click(object sender, RoutedEventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                epubPath.Content = openFileDialog.FileName;
+                updateCreateButton();
+            }
+        }
+
+        private void updateCreateButton()
+        {
+            createButton.IsEnabled = false;
+            if (string.IsNullOrEmpty(openFileDialog.FileName)) return;
+            if (string.IsNullOrEmpty(projName.Text)) return;
+            if (string.IsNullOrEmpty(folderBrowserDialog.SelectedPath)) return;
+            createButton.IsEnabled = true;
+        }
+    }
+}
+/*
+a-christmas-carol-audio.epub
+*/
---
+++ EPUBGenerator/EPUBGenerator/TestClass.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using eBdb.EpubReader;
+using System.Xml.Linq;
+using System.IO;
+
+namespace EPUBGenerator
+{
+    public class TestClass
+    {
+        public static string Create(String file)
+        {
+            //string path = @"C:\Users\xinghbtong.Baitongs\Documents\Top\Chula\Year 4\Senior Project\Project\EPUB\";
+            string path = "";
+            Epub epub = new Epub(path + file);
+            string output = "" + ((ContentData)epub.Content[2]).GetContentAsPlainText();
+            for (int i = 110; i < epub.Content.Count; i++)
+            {
+                string content = ((ContentData)epub.Content[i]).GetContentAsPlainText();
+                //output += "------------- [" + i + "]\r\n";
+                output += content + "\r\n";
+            }
+            return output;
+        }
+
+        private static string getContentData(XElement element)
+        {
+            string output = "";
+            foreach (XNode node in element.Nodes()) 
+            {
+                if (node is XText)
+                    output += ((XText)node).Value + "\r\n";
+                else if (node is XElement)
+                    output += getContentData((XElement)node);
+            }
+            return output;
+        }
+
+        private static string getAllContents(List<NavPoint> TOC, string savePath)
+        {
+            string output = "----------NavPoint----------\r\n";
+            foreach (NavPoint np in TOC)
+            {
+                output += "ID: " + np.ID + "\r\n";
+                output += "Title: " + np.Title + "\r\n";
+                output += "Source: " + np.Source + "\r\n";
+                output += "Order: " + np.Order + "\r\n";
+                output += "<<<<< CONTENT >>>>>\r\n";
+                if (np.ContentData != null)
+                {
+                    XElement root = XElement.Parse(np.ContentData.Content);
+                    XNamespace xns = root.Attribute("xmlns") != null ? root.Attribute("xmlns").Value : XNamespace.None;
+                    output += getContentData(root.Element(xns + "body"));
+                    Directory.CreateDirectory(savePath);
+                    Console.WriteLine(Path.Combine(savePath, np.Source));
+                    StreamWriter sw = new StreamWriter(Path.Combine(savePath, np.Source));
+                    sw.Write(root);
+                    sw.Close();
+                }
+                output += "<<<<< \\CONTENT >>>>>\r\n";
+                output += getAllContents(np.Children, savePath);
+            }
+            return output;
+        }
+
+        public static string reCreate(string file, string savePath)
+        {
+            //string path = @"C:\Users\xinghbtong.Baitongs\Documents\Top\Chula\Year 4\Senior Project\Project\EPUB\";
+            string path = "";
+            Epub epub = new Epub(path + file);
+            return getAllContents(epub.TOC, savePath);
+        }
+    }
+}
