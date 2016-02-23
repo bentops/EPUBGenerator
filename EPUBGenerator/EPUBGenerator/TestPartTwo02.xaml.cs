dbeb0568e4aa916247f737dbf7030277a1750a1b
Ben PS <bentops@gmail.com>
2016-02-23 20:52:40 +0700

    สร้างหน้าเปล่าเพื่อลองทำ PartII

---
+++ EPUBGenerator/EPUBGenerator/TestPartTwo02.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EPUBGenerator
+{
+    /// <summary>
+    /// Interaction logic for TestPartTwo02.xaml
+    /// </summary>
+    public partial class TestPartTwo02 : Window
+    {
+        public TestPartTwo02()
+        {
+            InitializeComponent();
+        }
+    }
+}
