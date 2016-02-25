260043ca43da39715e59dae2f1c167a2a2e88e32
Ben PS <bentops@gmail.com>
2016-02-25 14:26:35 +0700

    TestPartTwo02

--- EPUBGenerator/EPUBGenerator/TestPartTwo02.xaml.cs
+++ EPUBGenerator/EPUBGenerator/TestPartTwo02.xaml.cs
@@ -19,19 +19,55 @@ namespace EPUBGenerator
     /// </summary>
     public partial class TestPartTwo02 : Window
     {
+        int count = 0;
         public TestPartTwo02()
         {
             InitializeComponent();
+            Random rnd = new Random();
+
+            for (int j =0; j<4; j++)
+            {
+                Paragraph paragraph = new Paragraph();
+                for (int i = 0; i < 20; i++)
+                {
+                    Run run = new Run();
+                    run.Text = i + "create_from_c# ";
+                    run.Background = PickRandomBrush(rnd);
+                    run.MouseEnter += R01_MouseEnter;
+                    run.MouseLeave += R01_MouseLeave;
+                    run.MouseLeftButtonDown += R_MouseLeftButtonDown;
+                    run.Cursor = Cursors.Hand;
+                    paragraph.Inlines.Add(run);
+                }
+                richTextBox.Document.Blocks.Add(paragraph);
+            }
+            
+            
         }
 
         private void R01_MouseEnter(object sender, MouseEventArgs e)
         {
-            R01.Background = Brushes.Yellow;
+            (sender as Run).Background = Brushes.Yellow;
         }
 
         private void R01_MouseLeave(object sender, MouseEventArgs e)
         {
-            R01.ClearValue(Run.BackgroundProperty);
+            (sender as Run).ClearValue(Run.BackgroundProperty);
         }
+        private void R_MouseLeftButtonDown(object sender, MouseEventArgs e)
+        {
+            (sender as Run).Text = "AAAAA" + count++;
+        }
+
+        private Brush PickRandomBrush(Random rnd)
+        {
+            Brush result = Brushes.Transparent;
+            Type brushesType = typeof(Brushes);
+            System.Reflection.PropertyInfo[] properties = brushesType.GetProperties();
+            int random = rnd.Next(properties.Length);
+            result = (Brush)properties[random].GetValue(null, null);
+            return result;
+        }
+
     }
 }
