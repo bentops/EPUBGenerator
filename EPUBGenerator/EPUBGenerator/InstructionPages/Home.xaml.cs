9bae0094d79fca252b5c6983fcac45dace5d5d3d
Ben PS <bentops@gmail.com>
2016-03-17 15:51:31 +0700

    finish instruction Home

--- EPUBGenerator/EPUBGenerator/InstructionPages/Home.xaml.cs
+++ EPUBGenerator/EPUBGenerator/InstructionPages/Home.xaml.cs
@@ -24,5 +24,56 @@ namespace EPUBGenerator.InstructionPages
         {
             InitializeComponent();
         }
+
+        int state = 1;
+        private void instructionMenuMouseOver(object sender, RoutedEventArgs e)
+        {
+            string senderName = ((Border)sender).Name;
+            Brush bg = (SolidColorBrush)(new BrushConverter().ConvertFrom("#29d9c2"));
+            Brush fore = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FF2F2933"));
+            if (senderName == "menu1" && state == 1)
+            {
+                createNewProject.Foreground = Brushes.Gray;
+                createNewProjectTh.Foreground = Brushes.Gray;
+                Arr1.Foreground = bg;
+                state = 2;
+            }
+            else if (senderName == "menu1" && state == 2)
+            {
+                createNewProject.Foreground = fore;
+                createNewProjectTh.Foreground = fore;
+                Arr1.Foreground = Brushes.Gray;
+                state = 1;
+            }
+            else if (senderName == "menu2" && state == 1)
+            {
+                OpenEditProject.Foreground = Brushes.Gray;
+                OpenEditProjectTh.Foreground = Brushes.Gray;
+                Arr2.Foreground = bg;
+                state = 2;
+            }
+            else if (senderName == "menu2" && state == 2)
+            {
+                OpenEditProject.Foreground = fore;
+                OpenEditProjectTh.Foreground = fore;
+                Arr2.Foreground = Brushes.Gray;
+                state = 1;
+            }
+            else if (senderName == "menu3" && state == 1)
+            {
+                Export.Foreground = Brushes.Gray;
+                ExportTh.Foreground = Brushes.Gray;
+                Arr3.Foreground = bg;
+                state = 2;
+            }
+            else if (senderName == "menu3" && state == 2)
+            {
+                Export.Foreground = fore;
+                ExportTh.Foreground = fore;
+                Arr3.Foreground = Brushes.Gray;
+                state = 1;
+            }
+
+        }
     }
 }
