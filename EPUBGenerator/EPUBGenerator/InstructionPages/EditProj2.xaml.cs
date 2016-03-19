6455fb0b5c8353395555c9f6ccdf518b3c385706
Ben PS <bentops@gmail.com>
2016-03-19 14:46:29 +0700

    เกือบครบละ - no error

---
+++ EPUBGenerator/EPUBGenerator/InstructionPages/EditProj2.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EPUBGenerator.InstructionPages
+{
+    /// <summary>
+    /// Interaction logic for ExportProj3.xaml
+    /// </summary>
+    public partial class EditProj2 : Page
+    {
+        public EditProj2()
+        {
+            InitializeComponent();
+        }
+
+        private void homebutton_Click(object sender, RoutedEventArgs e)
+        {
+            InstructionSwitcher.Switch(InstructionSwitcher.home);
+        }
+
+        private void exitbutton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Windows[1].Close();
+        }
+
+        private void NextPage_Click(object sender, RoutedEventArgs e)
+        {
+            InstructionSwitcher.Switch(InstructionSwitcher.createProj2);
+        }
+
+        private void PrevPage_click(object sender, RoutedEventArgs e)
+        {
+            InstructionSwitcher.Switch(InstructionSwitcher.home);
+        }
+
+    }
+}
